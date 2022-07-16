9167dd8dac13b52d803cbbc81b4cde62843f3db3
Lukáš Hejtmánek <hejtmy@gmail.com>
2022-07-16 14:59:15 +0200

    Adds function to provide simple screenshot analyses

---
+++ R/fspr-analysis.R
@@ -0,0 +1,43 @@
+#' Creates a screenshot analysis summaries for
+#'
+#' @param fspr_obj fspr screenshots object
+#'
+#' @return data.frame
+#' @export
+#'
+#' @examples
+create_screenshot_summaries <- function(fspr_obj){
+  if(!("fspr.screenshots" %in% class(fspr_obj))){
+    warning("this is not a screenshot object")
+    return(NULL)
+  }
+  df_res <- data.frame()
+  for(i_screenshot in unique(fspr_obj$scene_analysis$iAnalysis)){
+    df_screenshot <- create_screenshot_summary(fspr_obj, i_screenshot)
+    df_res <- rbind(df_res, df_screenshot)
+  }
+  return(df_res)
+}
+
+create_screenshot_summary <- function(fspr_obj, i_screenshot){
+  # distance from all the objects
+  df_res <- filter(fspr_obj$scene_analysis,
+         iAnalysis== i_screenshot) %>%
+    left_join(select(fspr_obj$screen_position, -time),
+               by = c("iAnalysis", "object")) %>%
+    filter(object != "nothing")
+
+  camera_position <- filter(fspr_obj$position,
+         iAnalysis == i_screenshot) %>%
+    select(starts_with("position")) %>%
+    as.vector()
+
+  df_res <- fspr_obj$object_positions %>%
+    filter(object %in% df_res$object) %>%
+    rowwise() %>%
+    mutate(camera_distance = euclid_distance(c(position_x, position_y),
+                                      camera_position[1:2])) %>%
+  select(object, camera_distance) %>%
+  right_join(df_res, by="object")
+  return(df_res)
+}
