8567cbcc6e8dae476e4cfe481739b3a523213d57
Lukáš Hejtmánek <hejtmy@gmail.com>
2022-07-16 15:00:39 +0200

    Adds path visualisation plot

--- R/fspr-visualisation.R
+++ R/fspr-visualisation.R
@@ -17,10 +17,28 @@ plot_screenshot_scene <- function(fspr_obj, i_screenshot, width = 0.1){
     geom_rect(aes(xmin = screen_x - (ratio), xmax = screen_x + (ratio),
                   ymin = screen_y - (ratio), ymax = screen_y + (ratio))) +
     ggrepel::geom_text_repel(aes(label=object)) +
-    lims(x=c(-0.5,1.5),y=c(-0.5,1.5))
+    lims(x = c(-0.5, 1.5), y = c(-0.5, 1.5))
   return(plt)
 }
 
 plot_screenshot_analysis <- function(fspr_obj, i_screenshot){
 
+}
+
+#' Plots position and object data
+#'
+#' @param fspr_obj
+#'
+#' @return
+#' @export
+#'
+#' @examples
+plot_positions_and_path <- function(fspr_obj){
+  ggplot(fspr_obj$object_positions, aes(position_x, position_y)) +
+    geom_path(data = fspr_obj$position) +
+    geom_point() +
+    ggrepel::geom_text_repel(aes(label=object)) +
+    geom_point(aes(x = fspr_obj$position$position_x[1],
+                   y = fspr_obj$position$position_y[1]),
+               shape=18, size = 10)
 }
