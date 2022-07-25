df6038d7e067d913c9ccfd800d83df4529d2e3d4
Lukáš Hejtmánek <hejtmy@gmail.com>
2022-07-25 22:07:59 +0200

    Adds option to plot screenshots with the original image

--- R/fspr-visualisation.R
+++ R/fspr-visualisation.R
@@ -1,23 +1,31 @@
 #' plots position of objects on the screen with approximate size as per their
 #'  visual size
 #'
-#' @param fspr_obj
-#' @param i_screenshot
+#' @param fspr_obj object to plot
+#' @param i_screenshot which screenshot to plot
+#' @param include_screenshot should the oriignal screenshot be included
+#' @param screenshots_path if screenshot should be included, what folder are the
+#' screenshots placed in? It will be searched for automatically based on i_screenshot
 #'
 #' @return
 #' @export
 #'
 #' @examples
-plot_screenshot_scene <- function(fspr_obj, i_screenshot, width = 0.1){
+plot_screenshot_scene <- function(fspr_obj, i_screenshot, include_screenshot = FALSE,
+                                  screenshots_path = NULL){
   df_screenshot <- get_screenshot_object_screen_position(fspr_obj, i_screenshot)
-  w <- width
-  plt <- df_screenshot %>%
-  ggplot(aes(x = screen_x, y = screen_y, fill = object)) +
+  plt <- ggplot(df_screenshot, aes(x = screen_x, y = screen_y, fill = object))
+  if(include_screenshot){
+    g <- get_screenshot_raster(screenshots_path, i_screenshot)
+    plt <- plt +
+      annotation_custom(g, xmin=0, xmax=1, ymin=0, ymax=1)
+  }
+  plt <- plt +
     geom_point() +
     geom_rect(aes(xmin = screen_x - (ratio), xmax = screen_x + (ratio),
                   ymin = screen_y - (ratio), ymax = screen_y + (ratio))) +
     ggrepel::geom_text_repel(aes(label=object)) +
-    lims(x = c(-0.5, 1.5), y = c(-0.5, 1.5))
+    expand_limits(x = c(0,1), y=c(0,1))
   return(plt)
 }
 
@@ -49,3 +57,17 @@ plot_positions_and_path <- function(fspr_obj, i_screenshot = NULL){
   }
   return(plt)
 }
+
+get_screenshot_raster <- function(base_path, index){
+  ptr <- sprintf("HighresScreenshot%05d", index)
+  filepath <- list.files(base_path,pattern = ptr, full.names = TRUE)
+  if(length(filepath) == 0){
+    warning("There is no file ", filepath, " at ", base_path)
+    return(NULL)
+  }
+  png <- png::readPNG(filepath)
+  g <- grid::rasterGrob(png, interpolate=TRUE, width = unit(1, "npc"), height = unit(1, "npc"))
+  return(g)
+}
+
+
