b3376e811d5315dc85ecfd623369b54424c805ae
Lukáš Hejtmánek <hejtmy@gmail.com>
2023-01-14 22:01:53 +0100

    Adds non visible filtering

--- R/fspr-getters.R
+++ R/fspr-getters.R
@@ -1,19 +1,33 @@
 get_screenshot_data <- function(fspr_obj, i_screenshot){
-  res <- list()
-  res <- sapply(names(fspr_obj))
-  res$postition
+  UseMethod("get_screenshot_data")
 }
 
-get_screenshot_object_screen_position <- function(fspr_obj, i_screenshot){
-  df_position <- filter_screenshot(fspr_obj$screen_position, i_screenshot)
-  df_scene <- filter_screenshot(fspr_obj$scene_analysis, i_screenshot)
-  df_position <- df_position[df_position$object %in% df_scene$object,]
-  df_position <- merge(df_position, select(df_scene, object, nHits, ratio))
-  return(df_position)
+get_screenshot_data.fspr.screenshots <- function(fspr_obj, i_screenshot) {
+  new_obj <- fspr_obj
+  fields <- c("screen_position", "scene_analysis", "position")
+  for (field in fields) {
+    new_obj[[field]] <- filter_screenshot(fspr_obj[[field]], i_screenshot)
+  }
+  return(new_obj)
 }
-
 
 filter_screenshot <- function(obj, i_screenshot){
-  if(!("iAnalysis" %in% names(obj))) return(NULL)
-  return(obj[obj$iAnalysis == i_screenshot,])
+  if (!("iAnalysis" %in% names(obj))) return(NULL)
+  return(obj[obj$iAnalysis == i_screenshot, ])
+}
+
+
+#' @description This function takes as input an fspr object and filters
+#' it to include onlythe objects that are visible in the scene.
+#' It does this by taking the objects that are visible in the scene
+#'
+#' @param fspr_obj fspr object
+#' @return filtered fspr object
+#' @export 
+
+filter_non_visible <- function(fspr_obj) {
+  objects <- fspr_obj$scene_analysis$object
+  fspr_obj$screen_position <- filter(fspr_obj$screen_position, object %in% objects)
+  fspr_obj$object_positions <- filter(fspr_obj$object_position, object %in% objects)
+  return(fspr_obj)
 }
