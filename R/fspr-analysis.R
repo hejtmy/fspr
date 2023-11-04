c420160419a67496d01c03f66720a8d4c6203663
Lukáš Hejtmánek <hejtmy@gmail.com>
2023-11-04 18:00:06 +0100

    Tries to fix the problem with

--- R/fspr-analysis.R
+++ R/fspr-analysis.R
@@ -16,7 +16,6 @@ create_screenshot_summaries <- function(fspr_obj) {
   tProgress <- txtProgressBar(max = length(i_screenshots), style = 3)
   for (i in seq_len(length(i_screenshots))){
     df_screenshot <- create_screenshot_summary(fspr_obj, i_screenshots[i])
-    if (is.null(df_screenshot)) next()
     df_res <- rbind(df_res, df_screenshot)
     setTxtProgressBar(tProgress, i)
   }
@@ -26,30 +25,38 @@ create_screenshot_summaries <- function(fspr_obj) {
 
 #' @export
 #' @describeIn create_screenshot_summaries Creates a single summary based on
-#' given screenshot index
+#' given screenshot index. In case there are no objects to be matched (only nothing
+#' has been detected in the screenshot), returns zero line data.frame with proper names
 #'
 create_screenshot_summary <- function(fspr_obj, i_screenshot){
   # distance from all the objects
   df_res <- filter_screenshot(fspr_obj$scene_analysis, i_screenshot)
   if (is.null(df_res)) return(NULL)
+
   df_res <- df_res %>%
     left_join(select(fspr_obj$screen_position, -time),
-               by = c("iAnalysis", "object")) %>%
+              by = c("iAnalysis", "object")) %>%
     filter(object != "nothing")
 
-  if(nrow(df_res) == 0) return(NULL)
+  if (nrow(df_res) == 0) {
+    dummy_df <- mutate(df_res, camera_distance =  NA_real_)
+    return(dummy_df)
+  }
 
+  ## Camera position in the world at the time of the screenshot
   camera_position <- filter(fspr_obj$position, iAnalysis == i_screenshot) %>%
     select(starts_with("position")) %>%
     unlist()
 
-  df_positions <- fspr_obj$object_positions %>%
+  # Calculates distances of each objecttowards the camera
+  df_distances <- fspr_obj$object_positions %>%
     filter(object %in% df_res$object) %>%
     rowwise() %>%
     mutate(camera_distance = euclid_distance(c(position_x, position_y),
                                              camera_position[1:2])) %>%
     select(object, camera_distance) %>%
     ungroup()
-  df_res <- right_join(df_positions, df_res, by = "object")
+
+  df_res <- right_join(df_distances, df_res, by = "object")
   return(df_res)
 }
